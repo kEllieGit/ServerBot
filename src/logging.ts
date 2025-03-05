fa086a5842cc3bc2548016fca6083d80289d9458
Dutchy <jo0st.mine@gmail.com>
2025-03-05 19:08:24 +0100

    changed back to local stuff for dev

--- src/logging.ts
+++ src/logging.ts
@@ -2,8 +2,8 @@ import { TextChannel } from "discord.js";
 import { client } from "./index"
 
 const Logging = {
-    GUILD_ID: "811256944953262102",
-    LOG_CHANNEL_ID: "1341168478861922434",
+    GUILD_ID: "1341508196589633636",
+    LOG_CHANNEL_ID: "1341508197491540040",
 
     async log(message: string, channelId?: string) {
         console.log(message);
